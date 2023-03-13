bf8ed9ded424c66d111d73231abedb932c5b0d41
Waldis <gonzalezsuse92@gmail.com>
2023-03-13 15:34:16 -0600

    Initial commit

---
+++ ApiWeb/Controllers/v1/LibroController.cs
@@ -0,0 +1,43 @@
+﻿using Aplicacion.Feautres.Libros.Comandos.CrearLibro;
+using Aplicacion.Feautres.Libros.Comandos.EliminarLibro;
+using Aplicacion.Feautres.Libros.Comandos.ModificarLibro;
+using Aplicacion.Feautres.Libros.Consultas.OptenerLibroPorId;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiWeb.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    public class LibroController : BaseApiController
+    {
+        //Post 
+        [HttpPost]
+       public async Task<IActionResult> Post(CrearLibro comando)
+        {
+            return Ok(await Mediator.Send(comando));
+        }
+         
+        //Put 
+        [HttpPut]
+        public async Task<IActionResult> Put(int Id, ModificarLibro comando)
+        {
+            if (Id != comando.Id) 
+                return BadRequest();
+            return Ok(await Mediator.Send(comando));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete (int id )
+        {
+            return Ok(await Mediator.Send(new EliminarLibro  { Id = id }));
+        }
+
+        //Get 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(await Mediator.Send(new OptenerLibroId { Id = id }));
+        }
+    }
+}
+
---
+++ Aplicacion/Feautres/Libros/Comandos/ModificarLibro/ModificarLibroValidaciones.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacion.Feautres.Libros.Comandos.ModificarLibro
+{
+    public  class ModificarLibroValidaciones : AbstractValidator<ModificarLibro>
+    {
+        public ModificarLibroValidaciones() {
+
+
+            RuleFor(p => p.TituloLibro)
+                           .NotEmpty().WithMessage("{PropertyName} no puede ir vacio")
+                           .MaximumLength(200).WithMessage("{PropertyName} no debe de exceder de {MaxLength} caracteres");
+
+
+            RuleFor(p => p.CantidadPaginas)
+               .NotEmpty().WithMessage("{PropertyName} no puede ir vacio");
+
+            RuleFor(p => p.Editorial)
+               .NotEmpty().WithMessage("{PropertyName} no puede ir vacio");
+
+            RuleFor(p => p.CantidadPaginas)
+             .NotEmpty().WithMessage("{PropertyName} no puede ir vacio");
+
+
+        }
+
+    }
+}
